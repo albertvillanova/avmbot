8aae266d2ba2282dec96d0f0081444a3a07b628b
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2019-11-10 20:26:45 +0100

    [setup] Add install requires
    Close GH-2

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wikidatabot',
     version='0.0.1',
-    description='Bot for Wikidata'
+    description='Bot for Wikidata',
+    author='Albert Villanova del Moral',
+    packages=find_packages(),
+    install_requires=[
+        'pywikibot',
+    ],
 )
