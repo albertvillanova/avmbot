de85c65256ed49ffa39fe7387494f051be368385
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2019-11-11 15:09:23 +0100

    [tests] Add comment with items to be tested in wikidata/wikidata
    GH-1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
 
+# wikidata, wikidata
+# Do a test edit to Wikidata
+# testqid = 'Q4115189' # Wikidata sandbox
+# testproperty = 'P31' # instance of
+# testvalue = 'Q3938'  # Sandbox
+
+
 @pytest.fixture(scope='session')
 def wikidatabot():
     import wikidatabot
