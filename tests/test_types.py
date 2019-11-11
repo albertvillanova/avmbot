696a3761f7a56719bf483d1587605eba1786b49c
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2019-11-11 13:15:03 +0100

    [tests] Use test wikidata site
    GH-3

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,14 +1,19 @@
-import pywikibot
-from wikidatabot.types import Claim
+import wikidatabot
+
+wikidatabot.SITE = wikidatabot.pywikibot.Site('test', 'wikidata')
+wikidatabot.REPO = wikidatabot.SITE.data_repository()
 
 
 class TestClaim:
 
     def test_init(self):
-        claim = Claim(property='P488', item='Q50')
-        assert claim.property == 'P488'
-        assert isinstance(claim.value, pywikibot.page.ItemPage)
-        assert claim.value.getID() == 'Q50'
-        assert isinstance(claim._claim, pywikibot.page.Claim)
-        assert claim._claim.repo._BaseSite__code == 'wikidata'
+        from wikidatabot.types import Claim
+        claim_property = 'P115'  # 'P488'
+        claim_item = 'Q271'  # 'Q50'
+        claim = Claim(property=claim_property, item=claim_item)
+        assert claim.property == claim_property
+        assert isinstance(claim.value, wikidatabot.pywikibot.page.ItemPage)
+        assert claim.value.getID() == claim_item
+        assert isinstance(claim._claim, wikidatabot.pywikibot.page.Claim)
+        assert claim._claim.repo._BaseSite__code == 'test'  # 'wikidata'
         assert claim._claim.repo._BaseSite__family.name == 'wikidata'
