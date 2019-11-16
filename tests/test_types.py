4cf557009b5812823dff3e068bd617a9126f110e
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2019-11-16 17:50:13 +0100

    [tests] Test Item attributes
    GH-5

--- tests/test_types.py
+++ tests/test_types.py
@@ -12,3 +12,19 @@ class TestClaim:
         assert isinstance(claim._claim, wikidatabot.pywikibot.page.Claim)
         assert claim._claim.repo._BaseSite__code == 'test'  # 'wikidata'
         assert claim._claim.repo._BaseSite__family.name == 'wikidata'
+
+
+class TestItem:
+
+    def test_init(self, wikidatabot):
+        from wikidatabot.types import Item
+        item_id = 'Q68'
+        pywikibot_item = wikidatabot.pywikibot.ItemPage(wikidatabot.repo, item_id)
+        _ = pywikibot_item.get()
+        item = Item(pywikibot_item)
+        assert item.id == pywikibot_item.id
+        assert item.labels == pywikibot_item.labels
+        assert item.descriptions == pywikibot_item.descriptions
+        assert item.aliases == pywikibot_item.aliases
+        assert item.statements == pywikibot_item.claims
+        assert item.sitelinks == pywikibot_item.sitelinks
