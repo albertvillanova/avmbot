f527fcf07d95862068b4edf64ad899ac455dceb7
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-02-11 15:30:10 +0100

    [scripts] Test new case in parse_position_value
    GH-30

--- tests/test_scripts/test_transfer_infotable.py
+++ tests/test_scripts/test_transfer_infotable.py
@@ -16,6 +16,7 @@ MAYOR_OF_A_CORUNA = 'Q12391919'
 MAYOR_OF_LA_VALL_D_UIXO = 'Q26693191'
 MEMBER_OF_THE_PARLIAMENT_OF_CATALONIA = 'Q18714088'
 MEMBER_OF_THE_SENATE_OF_SPAIN = 'Q19323171'
+MINISTER_OF_FOREIGN_AFFAIRS_OF_SPAIN = 'Q32969906'
 MINISTER_OF_THE_NAVY = 'Q15895305'
 PARLIAMENT_OF_CATALONIA = 'Q135630'
 PRESIDENT_OF_COLOMBIA = 'Q853475'
@@ -34,7 +35,9 @@ class TestScriptTransferInfotable:
         ("[[Llista de presidents de Colòmbia|President de Colòmbia]]", PRESIDENT_OF_COLOMBIA, []),
         ("[[President del Consell de Castella]]", PRESIDENT_OF_THE_COUNCIL_OF_CASTILE, []),
         ("Ambaixador del regne d'Espanya al Regne de França", AMBASSADOR_OF_SPAIN_TO_FRANCE, []),
-        ("[[Capità general de València]]", GENERAL_CAPTAIN_OF_VALENCIA, [])
+        ("[[Capità general de València]]", GENERAL_CAPTAIN_OF_VALENCIA, []),
+        ("[[Llista dels Ministres d'Afers Exteriors d'Espanya|Ministre d'Estat]]", MINISTER_OF_FOREIGN_AFFAIRS_OF_SPAIN,
+         []),
     ])
     def test_parse_position_value(self, position_value, expected_claim_id, expected_qualifiers):
         position_claim, qualifiers = parse_position_value(position_value)
