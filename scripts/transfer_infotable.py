0dede9a1e23661963f291bbeb7c2c7684669b008
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-02-27 08:10:26 +0100

    [scripts] Minor changes
    GH-27

--- scripts/transfer_infotable.py
+++ scripts/transfer_infotable.py
@@ -749,7 +749,7 @@ def add_statements(item, statements, summary=''):
 
 
 def add_statement(item, new_statement, summary=''):
-    logger.info(f"Try to add new statement: {new_statement}")
+    logger.info(f"Try to add new statement: {new_statement.claim.value.id}")
     duplicated = check_duplicate(item, new_statement, summary=summary)
     if not duplicated:
         logger.info(f"Add statement: {new_statement}")
